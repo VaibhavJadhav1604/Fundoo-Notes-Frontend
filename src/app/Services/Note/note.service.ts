b74bd82a9a3fa88e34fbc66d6d8df59421f37ea6
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-08 20:53:45 +0530

    Add files via upload

---
+++ src/app/Services/Note/note.service.ts
@@ -0,0 +1,43 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpService } from '../Http/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+  token:any;
+  constructor(private httpService:HttpService) {this.token=localStorage.getItem('token') }
+
+  AddNotes(reqData:any)
+  {
+    let header={
+      headers:new HttpHeaders({
+        'Content-type':'application/json',
+        'Authorization':'Bearer '+this.token
+      })
+    }
+    return this.httpService.postServiceReset('/Note/AddNotes',reqData,true,header);
+  }
+
+  GetAllNotes(){
+    let header = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' +this.token
+      })
+    }
+    return this.httpService.getService('/Note/GetAllNotes',true,header);
+  }
+
+  UpdateNotes(reqData:any)
+  {
+    let header={
+      headers:new HttpHeaders({
+        'Content-Type':'Application/json',
+        'Authorization':'Bearer '+ this.token
+      })
+    }
+    return this.httpService.putService('/Note/UpdateNotes?NoteId='+reqData.noteId,reqData,true,header);
+  }
+}
