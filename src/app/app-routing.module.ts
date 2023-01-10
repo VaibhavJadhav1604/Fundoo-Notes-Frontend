6356ee68b1c80d695f5e7f6b20291ee0ee72ad9d
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-10 08:57:51 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,30 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { DbComponent } from './Components/db/db.component';
-import { CreateNoteComponent } from './Components/create-note/create-note.component';
+import { DemoComponent } from './Components/demo/demo.component';
+import { AuthGuardGuard } from './Auth/auth-guard.guard';
+import { NotesComponent } from './Components/notes/notes.component';
+import { DisplaynotesComponent } from './Components/displaynotes/displaynotes.component';
+import { DemologinComponent } from './Component/demologin/demologin.component';
+import { TrashnotesComponent } from './Components/trashnotes/trashnotes.component';
+import { ArchivenotesComponent } from './Components/archivenotes/archivenotes.component';
 
 const routes: Routes = [
-  {path:'Login',component:LoginComponent},
-  {path:'Register',component:RegisterComponent},
-  {path:'ForgotPassword',component:ForgotPasswordComponent},
-  {path:'ResetPassword/:token',component:ResetPasswordComponent},
-  {path:'Dashboard',component:DashboardComponent},
-  {path:'db',component:DbComponent},
-  {path:'createnote',component:CreateNoteComponent}
+  { path: '', redirectTo: "/Login", pathMatch: 'full' },
+  { path: 'Login', component: LoginComponent },
+  { path: 'demologin', component: DemologinComponent },
+  { path: 'Register', component: RegisterComponent },
+  { path: 'ForgotPassword', component: ForgotPasswordComponent },
+  { path: 'ResetPassword/:token', component: ResetPasswordComponent },
+  { path: 'Display', component: DisplaynotesComponent },
+  {
+    path: 'Dashboard', component: DashboardComponent, canActivate: [AuthGuardGuard],
+    children: [
+      { path: 'notes', component: NotesComponent },
+      { path: 'trash', component: TrashnotesComponent },
+      { path: 'archive', component: ArchivenotesComponent }
+    ]
+  }
 ];
 
 @NgModule({
