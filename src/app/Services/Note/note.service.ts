ad8b23c0e30d11f529e5b128c410d3f36d8296eb
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-10 08:53:38 +0530

    Add files via upload

--- src/app/Services/Note/note.service.ts
+++ src/app/Services/Note/note.service.ts
@@ -40,4 +40,37 @@ export class NoteService {
     }
     return this.httpService.putService('/Note/UpdateNotes?NoteId='+reqData.noteId,reqData,true,header);
   }
+
+  TrashNotes(reqData:any)
+  {
+    let header={
+      headers:new HttpHeaders({
+        'Content-Type':'Application/json',
+        'Authorization':'Bearer '+ this.token
+      })
+    }
+    return this.httpService.putService('/Note/IsTrash?NoteId='+reqData.noteId,reqData,true,header);
+  }
+
+  ArchiveNotes(reqData:any)
+  {
+    let header={
+      headers:new HttpHeaders({
+        'Content-Type':'Application/json',
+        'Authorization':'Bearer '+ this.token
+      })
+    }
+    return this.httpService.putService('/Note/IsArchive?NoteId='+reqData.noteId,reqData,true,header);
+  }
+
+  DeleteNote(reqData:any)
+  {
+    let header={
+      headers:new HttpHeaders({
+        'Content-Type':'Application/json',
+        'Authorization':'Bearer '+ this.token
+      })
+    }
+    return this.httpService.deleteService("/Note/DeleteTrashedNote?NoteId="+reqData.noteId,reqData,true,header);
+  }
 }
