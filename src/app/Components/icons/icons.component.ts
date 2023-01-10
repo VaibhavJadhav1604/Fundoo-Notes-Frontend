549ab5c6291e8eebddd06a7bac2e4b2e03e0cec7
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-10 08:54:31 +0530

    Add files via upload

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { NoteService } from 'src/app/Services/Note/note.service';
 import { ArchivenotesComponent } from '../archivenotes/archivenotes.component';
@@ -9,92 +10,89 @@ import { TrashnotesComponent } from '../trashnotes/trashnotes.component';
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss']
 })
-export class IconsComponent implements OnInit{
-@Input() trashnotecard:any;
-@Output() iconEvent = new EventEmitter<string>();
-constructor(private notes:NoteService,private route:ActivatedRoute){}
+export class IconsComponent implements OnInit {
 
-istrash:boolean=false;
-isarchive:boolean=false;
+  @Input() trashnotecard: any;
+  @Output() iconEvent = new EventEmitter<string>();
+  constructor(private notes: NoteService, private route: ActivatedRoute,private snackbar:MatSnackBar) { }
+
+  istrash: boolean = false;
+  isarchive: boolean = false;
   ngOnInit(): void {
     let comp = this.route.snapshot.component;
 
-    if(comp == TrashnotesComponent){
+    if (comp == TrashnotesComponent) {
       this.istrash = true;
-    } 
+    }
 
-    if(comp == ArchivenotesComponent){
+    if (comp == ArchivenotesComponent) {
       this.isarchive = true;
-    } 
+    }
   }
 
-  trashnote()
-  {
-    let reqData={
-    noteId:[this.trashnotecard.noteId],
+  trashnote() {
+    let reqData = {
+      noteId: [this.trashnotecard.noteId],
     }
 
     console.log(reqData);
-    this.notes.TrashNotes(reqData).subscribe((Response:any)=>
-    {
+    this.notes.TrashNotes(reqData).subscribe((Response: any) => {
       console.log(Response);
       this.iconEvent.emit(Response);
     })
+    this.snackbar.open("Note Trashed SuccessFully",'',{duration:2000});
   }
 
-  restorenote()
-  {
-    let reqData={
-    noteId:[this.trashnotecard.noteId],
+  restorenote() {
+    let reqData = {
+      noteId: [this.trashnotecard.noteId],
     }
 
     console.log(reqData);
-    this.notes.TrashNotes(reqData).subscribe((Response:any)=>
-    {
+    this.notes.TrashNotes(reqData).subscribe((Response: any) => {
       console.log(Response);
       this.iconEvent.emit(Response);
     })
+    this.snackbar.open("Note Restored SuccessFully",'',{duration:2000});
   }
 
-  deletenote()
-  {
-    let reqData={
-      noteId:[this.trashnotecard.noteId],
+  deletenote() {
+    let reqData = {
+      noteId: [this.trashnotecard.noteId],
     }
 
     console.log(reqData);
-    this.notes.DeleteNote(reqData).subscribe((Response:any)=>
-    {
+    this.notes.DeleteNote(reqData).subscribe((Response: any) => {
       console.log(Response);
       this.iconEvent.emit(Response);
     })
+    this.snackbar.open("Note Deleted SuccessFully",'',{duration:2000});
   }
 
-  archivenote()
-  {
-    let reqData={
-    noteId:[this.trashnotecard.noteId],
+  archivenote() {
+    let reqData = {
+      noteId: [this.trashnotecard.noteId],
     }
 
     console.log(reqData);
-    this.notes.ArchiveNotes(reqData).subscribe((Response:any)=>
-    {
+    this.notes.ArchiveNotes(reqData).subscribe((Response: any) => {
       console.log(Response);
       this.iconEvent.emit(Response);
     })
+    this.snackbar.open("Note Archived SuccessFully",'',{duration:2000});
   }
 
-  unarchivenote()
-  {
-    let reqData={
-    noteId:[this.trashnotecard.noteId],
+  unarchivenote() {
+    let reqData = {
+      noteId: [this.trashnotecard.noteId],
     }
 
     console.log(reqData);
-    this.notes.ArchiveNotes(reqData).subscribe((Response:any)=>
-    {
+    this.notes.ArchiveNotes(reqData).subscribe((Response: any) => {
       console.log(Response);
       this.iconEvent.emit(Response);
     })
+    this.snackbar.open("Note UnArchived SuccessFully",'',{duration:2000});
   }
 }
+
